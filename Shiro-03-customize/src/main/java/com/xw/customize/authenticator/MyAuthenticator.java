2b58d119000f6cd3284e29e0c3c97976ff7bd17d
XisWho <2210845633@qq.com>
2023-08-23 14:14:21 +0800

    Add comment

--- Shiro-03-customize/src/main/java/com/xw/customize/authenticator/MyAuthenticator.java
+++ Shiro-03-customize/src/main/java/com/xw/customize/authenticator/MyAuthenticator.java
@@ -25,6 +25,7 @@ public class MyAuthenticator extends ModularRealmAuthenticator {
         assertRealmsConfigured();
         Collection<Realm> realms = getRealms();
         String type = ((UsernamePasswordTypeToken) authenticationToken).getType();
+        // 这里实现的效果是根据传递的type参数区分不同等级的用户：NORMAL，VIP，然后再交由不同的AuthenticatingRealm进行认证。
         List<Realm> filterRealmes = realms.stream().filter(realm -> type.equals(realmTypeMap.get(realm.getName()))).collect(Collectors.toList());
         System.out.println(filterRealmes.stream().map(Realm::getName).reduce((name1, name2) -> name1+", "+name2).get() + " authenticate " + type);
         if (filterRealmes.size() == 1) {
